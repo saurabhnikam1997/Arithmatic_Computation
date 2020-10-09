0a7766f7c6548262ab432584b0d1d45bb634ebee
saurabhnikam1997 <nikam280197@gmail.com>
2020-10-09 22:55:39 +0530

    Calculate first expression

--- Arithmatic_Computation_Problem.sh
+++ Arithmatic_Computation_Problem.sh
@@ -3,7 +3,17 @@
 echo "Arithmatic Computation Problem"
 
 #UC_1_Take three inputs from user
-
+function input
+{
 read -p "Enter First Number" A
 read -p "Enter Second Number" B
 read -p "Enter Third Number" C
+}
+
+
+function firstEquation()
+{
+	input
+	firstEquationResult=$(( $A + $B * $C ))
+}
+firstEquation
