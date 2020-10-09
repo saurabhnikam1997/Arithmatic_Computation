f0272d16182bc2080d612dffe9228bae63d8a9bd
saurabhnikam1997 <nikam280197@gmail.com>
2020-10-09 22:36:26 +0530

    Taken Three inputs from user

--- Arithmatic_Computation_Problem.sh
+++ Arithmatic_Computation_Problem.sh
@@ -1,3 +1,9 @@
 #!/bin/bash -x
 
 echo "Arithmatic Computation Problem"
+
+#UC_1_Take three inputs from user
+
+read -p "Enter First Number" firstNumber
+read -p "Enter Second Number" secondNumber
+read -p "Enter Third Number" thirdNumber
