bfe9ff99b6e752686ea4cd667fab919bfef741fc
saurabhnikam1997 <nikam280197@gmail.com>
2020-10-09 22:41:05 +0530

    variable changed

--- Arithmatic_Computation_Problem.sh
+++ Arithmatic_Computation_Problem.sh
@@ -4,6 +4,6 @@ echo "Arithmatic Computation Problem"
 
 #UC_1_Take three inputs from user
 
-read -p "Enter First Number" firstNumber
-read -p "Enter Second Number" secondNumber
-read -p "Enter Third Number" thirdNumber
+read -p "Enter First Number" A
+read -p "Enter Second Number" B
+read -p "Enter Third Number" C
