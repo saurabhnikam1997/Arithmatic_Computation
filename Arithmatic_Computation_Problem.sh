d12b519706c4c2c688c998adf976f95e66ac1310
saurabhnikam1997 <nikam280197@gmail.com>
2020-10-10 07:23:59 +0530

    Stored result in dictionary

--- Arithmatic_Computation_Problem.sh
+++ Arithmatic_Computation_Problem.sh
@@ -2,45 +2,57 @@
 
 echo "Arithmatic Computation Problem"
 
+declare -A storeResult
+storeResult=(["result1"]=0 ["result2"]=0 ["result3"]=0 ["result4"]=0 )
+
 #UC_1_Take three inputs from user
-function input
+function getInputs()
 {
-read -p "Enter First Number" A
-read -p "Enter Second Number" B
-read -p "Enter Third Number" C
+	read -p "Enter First Number" A
+	read -p "Enter Second Number" B
+	read -p "Enter Third Number" C
+
+firstEquation
+secondEquation
+thirdEquation
+fourthEquation
 }
 
 
 function firstEquation()
 {
-	input
+	# Expression a+b*c
 	firstEquationResult=$(( $A + $B * $C ))
+	storeResult[result1]=$firstEquationResult
 }
-firstEquation
+
+#firstEquation
 
 function secondEquation()
 {
-	input
+	# Expression a*b+c
 	secondEquationResult=$(( $A * $B + $C ))
+	storeResult[result2]=$secondEquationResult
 }
-secondEquation
+#secondEquation
 
 function thirdEquation()
 {
-	input
+	# Expression c+a/b
+
 	thirdEquationResult=`expr "scale=3; $C +$A / $B "|bc`
+	storeResult[result3]=$thirdEquationResult
 }
-thirdEquation
+#thirdEquation
 
 
 function fourthEquation()
 {
-	input
+	# Expression a%b+c
 	fourthEquationResult=$(( $A % $B +$C ))
+	storeResult[result4]=$fourthEquationResult
 }
-fourthEquation
-
-
+getInputs
 
 
 
